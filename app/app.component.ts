d186e8aad5944ac46439fde03e5560b473780735
rrojas <rrojas@RROJAS>
2017-05-29 09:38:46 -0500

    commit changes

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,7 +3,10 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'pm-app',
     template: `
-        <h1>Angular2: Getting Started</h1>
+        <h1>Angular2: Getting Started</h1><br/>
+        <h3>{{pageTitle}}</h3>
     `
 })
-export class AppComponent { }
+export class AppComponent { 
+    pageTitle : string = 'Roy Test pageTitle';
+}
