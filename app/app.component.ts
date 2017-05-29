b9255e32b7f738568f5dd8d5b0b792f103bef3b4
rrojas <rrojas@RROJAS>
2017-05-29 14:45:35 -0500

    adding routin

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,12 +3,24 @@ import { ProductService } from './products/product.service';
 @Component({
     selector: 'pm-app',
     template: `
-        <div class="container">
-            <h1>Angular2: Getting Started</h1><br/>
-            <pm-products></pm-products>
+        <div>
+            <nav class='navbar navbar-default'>
+                <div class='container-fluid'>
+                    <a class='navbar-brand'>{{pageTitle}}</a>
+                    <ul class='nav navbar-nav'>
+                        <li><a [routerLink]="['/welcome']">Home</a></li>
+                        <li><a [routerLink]="['/products']">Product List</a></li>
+                    </ul>
+                   
+                </div>
+            </nav>
+            <div class="container">
+                <router-outlet></router-outlet>
+             </div>
         </div>
     `,
     providers : [ProductService]
 })
-export class AppComponent { 
+export class AppComponent {
+        pageTitle : string = "@ngular2: Getting Started" 
 }
